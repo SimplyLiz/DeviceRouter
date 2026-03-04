ef9f5666667c9c87f0689219a4a26e0146e163ef
Lisa <lisa.welsch1985@gmail.com>
2026-03-04 18:10:22 +0100

    Exclude e2e/ from ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['**/dist/', '**/coverage/', '**/node_modules/', 'examples/', '**/scripts/'],
+    ignores: ['**/dist/', '**/coverage/', '**/node_modules/', 'examples/', '**/scripts/', 'e2e/'],
   },
   {
     rules: {
